1fa5545b55d83e76fbc02f00d165760f3282ccb2
Gab <gab.github@gmail.com>
2017-01-15 14:53:30 -0500

    Creation of the data gatherer module and target temperature.

---
+++ dataGatherer/gatherer.py
@@ -0,0 +1,8 @@
+class Gatherer:
+    """
+    This class will gather data from various sensors of the SenseHat and other various sources from the RaspberryPi.
+    The date is mostly related to temperature and the RasberryPi CPU usage.
+    """
+
+    def __init__(self):
+        print("Hello world!")
---
+++ targetTemp/target_temp.py
@@ -0,0 +1,25 @@
+class TargetTemperature:
+    """
+    This class would normally be for obtaining temperature from a reliable temperature sensor that is external to the
+    RaspberryPi and the SensorHat. However, since I do not currently have one that allow me to easily obtain
+    temperature, I will rely on an user configurable target temperature for now.
+    """
+    type = 'Celsius'
+
+    def __init__(self, temperature):
+        self.temperature = temperature
+
+    def get_temperature(self):
+        return self.temperature
+
+    def set_temperature(self, temperature):
+        """
+        Set the temperature (in Celsius) only if it is between 0 and 70, according to the operating temperature of the
+        RaspberryPi as specified at : https://www.raspberrypi.org/help/faqs/#performanceOperatingTemperature
+        :param temperature:
+        :return:
+        """
+        if (temperature >= 0 and temperature <= 70):
+            self.temperature = temperature
+
+        return self.temperature
