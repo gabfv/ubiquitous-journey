9e85f4f24c3bf5af92a2190668dae945762af9e1
Gab <gab.github@gmail.com>
2017-01-16 11:20:26 -0500

    Renamed the package and created the class.

---
+++ manager/manager.py
@@ -0,0 +1,7 @@
+class Manager:
+    """
+    This is to manage the interactions between the joystick, screen and the data gatherer.
+    """
+
+    def __init__(self):
+        print("Hello World!")
