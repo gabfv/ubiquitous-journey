c7f0d6b9cf7b825efc972f341f1fb011aa9dc06c
Gab <gab.github@gmail.com>
2017-01-16 11:24:14 -0500

    Added the main loop

--- manager/manager.py
+++ manager/manager.py
@@ -1,7 +1,15 @@
+from sense_hat import SenseHat
+
+
 class Manager:
     """
     This is to manage the interactions between the joystick, screen and the data gatherer.
     """
 
     def __init__(self):
-        print("Hello World!")
+        self.sense_hat = SenseHat()
+
+    def main_loop(self):
+        while True:
+            print("TDB")
+            # TODO : continue with what's needed
